c25c721b12d6223cae041ad2abee10473d7e6e1c
Matej-ch <matejchalachan@gmail.com>
2016-01-16 16:33:33 +0100

    oprava chyb verzia 2

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-Horymír padá, když by se skript pustil 10.3. 2016. -- 
+Horymír padá, když by se skript pustil 10.3. 2016. -- hotovo
+
 Rubocop padá. -- 
+
 bylo by fajn kód strukturovat alespoò do pár metod a víc používat pomocné promìnné (tøeba
-name_date[ARGV[0]]). -- hotovo 
+name_date[ARGV[0]]). -- hotovo
+
 Když je `File.open` mìl by být i `File.close`,
 nebo použít verzi s blockem, nebo `File.read` -- hotovo
+
+Rubocop vystup (testovane na windows):
--- kolikjeste.rb
+++ kolikjeste.rb
@@ -11,58 +11,58 @@ def read_file(file,type)
   names_hash
 end
 
-def current_year
+def current_year(year_string)
   date = Time.new
-  year_now = date.strftime('%Y')
+  date.strftime(year_string)
 end
 
 def current_date(date_string)
   date = Time.new
-  current_date = date.strftime(date_string)
+  date.strftime(date_string)
+end
+
+def difference(start_date, end_date)
+  s_date = Date.parse start_date
+  e_date =  Date.parse end_date
+  (e_date - s_date).to_i
+end
+
+def get_correct_year(current, month, day)
+  year = current.to_i + 1
+  flag_leap = Date.leap?(year)
+  until flag_leap
+    year = year.to_i + 1
+    flag_leap = Date.leap?(year)
+  end if month.to_i == 2 && day.to_i == 29
+  year
 end
 
 def name_date(name)
-
   all_names = read_file('svatky.txt','r')
-=begin
-  current_date = date.strftime('%Y.%m.%d.')
-current_date = date.strftime('2016.3.10.')
-=end
-  date = current_date('2016.3.10.')
-  year = current_year
-  puts date
-  puts year
+  date = current_date('%Y.%m.%d.')
+  year = current_year('%Y')
 
   name_date = all_names.select { |key, value| key == name}
-  puts name_date
 
   if name_date.any?
     flip_name_date = year + '.' +
         name_date[name].split('.')[1] +'.' +
         name_date[name].split('.')[0]+'.'
 
-    puts flip_name_date
+    month = name_date[name].split('.')[1]
+    day = name_date[name].split('.')[0]
 
-    start_date = Date.parse date
-    puts start_date
-
-    end_date =  Date.parse flip_name_date
-    puts end_date
-
-    date_difference = (end_date - start_date).to_i
+    date_difference = difference(date,flip_name_date)
 
     if date_difference == 0
       puts 'Congratulations'
     elsif date_difference < 0
-      year = year.to_i + 1
+      year = get_correct_year(year, month, day)
 
-      puts Date.leap?(year)
-
-      #abort
-      flip_name_date = year.to_s + '.' + name_date[name].split('.')[1] +'.'+ name_date[name].split('.')[0]+'.'
-      start_date = Date.parse date
-      end_date =  Date.parse flip_name_date
-      date_difference = (end_date - start_date).to_i
+      flip_name_date = year.to_s + '.' +
+          name_date[name].split('.')[1] +'.'+ 
+          name_date[name].split('.')[0]+'.'
+      date_difference = difference(date,flip_name_date)
       puts "You have to wait #{date_difference} more days"
     else
       puts "You have to wait #{date_difference} more days"
