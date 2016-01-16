4503ad751669b6b30e561640e71822555381ac25
Matej-ch <matejchalachan@gmail.com>
2016-01-16 19:42:46 +0100

    oprava chyb verzia 3

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+Vypracované a testované na windows systéme
+
 Horymír padá, když by se skript pustil 10.3. 2016. -- hotovo
 
-Rubocop padá. -- 
+Rubocop padá. -- hotovo nepada, problem s parser (2.2.3.0) bol nastaveny 2.2.6.0
 
 bylo by fajn kód strukturovat alespoò do pár metod a víc používat pomocné promìnné (tøeba
 name_date[ARGV[0]]). -- hotovo
@@ -8,4 +10,10 @@ name_date[ARGV[0]]). -- hotovo
 Když je `File.open` mìl by být i `File.close`,
 nebo použít verzi s blockem, nebo `File.read` -- hotovo
 
-Rubocop vystup (testovane na windows):
+Rubocop vystup (testovane na windows): Running RuboCop...
+                                       Inspecting 1 file
+                                       .
+                                       
+                                       1 file inspected, no offenses detected
+                                       
+Najviac problemov bolo s Assigment Branch Condition size for "method_name" is too high
--- kolikjeste.rb
+++ kolikjeste.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require 'date'
 
-def read_file(file,type)
-  names_hash = Hash.new
+def read_file(file, type)
+  names_hash = {}
   f = File.open(file, type)
   f.readlines.each do |row|
     names_hash[row.to_s.split(' ')[1]] = row.to_s.split(' ')[0]
@@ -11,14 +11,9 @@ def read_file(file,type)
   names_hash
 end
 
-def current_year(year_string)
+def current_year_date
   date = Time.new
-  date.strftime(year_string)
-end
-
-def current_date(date_string)
-  date = Time.new
-  date.strftime(date_string)
+  [date.strftime('%Y'), date.strftime('%Y.%m.%d.')]
 end
 
 def difference(start_date, end_date)
@@ -34,39 +29,35 @@ def get_correct_year(current, month, day)
     year = year.to_i + 1
     flag_leap = Date.leap?(year)
   end if month.to_i == 2 && day.to_i == 29
-  year
+  year.to_s
+end
+
+def check_difference(date_difference)
+  puts 'Congratulations' if date_difference == 0
+  if date_difference < 0
+    year_new = get_correct_year(current_year_date[0], month_day[0], month_day[1])
+    date_difference = difference(current_year_date[1], "#{year_new}.#{month_day[0]}.#{month_day[0]}.")
+    puts "You have to wait #{date_difference} more days"
+  elsif date_difference > 0
+    puts "You have to wait #{date_difference} more days"
+  end
+end
+
+def month_day
+  name_date = read_file('svatky.txt', 'r').select { |key, _value| key == ARGV[0] }
+  return name_date[ARGV[0]].split('.')[1], name_date[ARGV[0]].split('.')[0]
+end
+
+def flip_date
+  name_date = read_file('svatky.txt', 'r').select { |key, _value| key == ARGV[0] }
+  "#{current_year_date[0]}.#{name_date[ARGV[0]].split('.')[1]}.#{name_date[ARGV[0]].split('.')[0]}."
 end
 
 def name_date(name)
-  all_names = read_file('svatky.txt','r')
-  date = current_date('%Y.%m.%d.')
-  year = current_year('%Y')
-
-  name_date = all_names.select { |key, value| key == name}
-
+  name_date = read_file('svatky.txt', 'r').select { |key, _value| key == name }
   if name_date.any?
-    flip_name_date = year + '.' +
-        name_date[name].split('.')[1] +'.' +
-        name_date[name].split('.')[0]+'.'
-
-    month = name_date[name].split('.')[1]
-    day = name_date[name].split('.')[0]
-
-    date_difference = difference(date,flip_name_date)
-
-    if date_difference == 0
-      puts 'Congratulations'
-    elsif date_difference < 0
-      year = get_correct_year(year, month, day)
-
-      flip_name_date = year.to_s + '.' +
-          name_date[name].split('.')[1] +'.'+ 
-          name_date[name].split('.')[0]+'.'
-      date_difference = difference(date,flip_name_date)
-      puts "You have to wait #{date_difference} more days"
-    else
-      puts "You have to wait #{date_difference} more days"
-    end
+    date_difference = difference(current_year_date[1], flip_date)
+    check_difference(date_difference)
   else
     puts 'We are sorry but we don\'t know when you should celebrate'
   end
