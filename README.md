d5084227f591b30997241f5f946f8f0bdf8280fd
Matej-ch <matejchalachan@gmail.com>
2016-01-16 15:37:02 +0100

    oprava chyb verzia 1

---
+++ README.md
@@ -0,0 +1,6 @@
+Horymír padá, když by se skript pustil 10.3. 2016. -- 
+Rubocop padá. -- 
+bylo by fajn kód strukturovat alespoò do pár metod a víc používat pomocné promìnné (tøeba
+name_date[ARGV[0]]). -- hotovo 
+Když je `File.open` mìl by být i `File.close`,
+nebo použít verzi s blockem, nebo `File.read` -- hotovo
--- kolikjeste.rb
+++ kolikjeste.rb
@@ -1,37 +1,75 @@
 #!/usr/bin/env ruby
 require 'date'
 
-names_hash = Hash.new
-date = Time.new
-File.open('svatky.txt', 'r').readlines.each do |line|
-  names_hash[line.to_s.split(' ')[1]] = line.to_s.split(' ')[0]
-end
-
-current_date = date.strftime('%Y.%m.%d.')
-year_now = date.strftime('%Y')
-
-name_date = names_hash.select { |key, value| key == ARGV[0]}
-
-if name_date.any?
-  flip_name_date = year_now + '.' + name_date[ARGV[0]].split('.')[1] +'.'+ name_date[ARGV[0]].split('.')[0]+'.'
-  start_date = Date.parse current_date
-  end_date =  Date.parse flip_name_date
-  date_difference = (end_date - start_date).to_i
-
-  if date_difference == 0
-    puts 'Congratulations'
-  elsif date_difference < 0
-    year_now = year_now.to_i + 1
-    flip_name_date = year_now.to_s + '.' + name_date[ARGV[0]].split('.')[1] +'.'+ name_date[ARGV[0]].split('.')[0]+'.'
-    start_date = Date.parse current_date
-    end_date =  Date.parse flip_name_date
-    date_difference = (end_date - start_date).to_i
-    puts "You have to wait #{date_difference} more days"
-  else
-    puts "You have to wait #{date_difference} more days"
+def read_file(file,type)
+  names_hash = Hash.new
+  f = File.open(file, type)
+  f.readlines.each do |row|
+    names_hash[row.to_s.split(' ')[1]] = row.to_s.split(' ')[0]
   end
-
-else
-  puts 'We are sorry but we don\'t know when you should celebrate'
+  f.close
+  names_hash
 end
 
+def current_year
+  date = Time.new
+  year_now = date.strftime('%Y')
+end
+
+def current_date(date_string)
+  date = Time.new
+  current_date = date.strftime(date_string)
+end
+
+def name_date(name)
+
+  all_names = read_file('svatky.txt','r')
+=begin
+  current_date = date.strftime('%Y.%m.%d.')
+current_date = date.strftime('2016.3.10.')
+=end
+  date = current_date('2016.3.10.')
+  year = current_year
+  puts date
+  puts year
+
+  name_date = all_names.select { |key, value| key == name}
+  puts name_date
+
+  if name_date.any?
+    flip_name_date = year + '.' +
+        name_date[name].split('.')[1] +'.' +
+        name_date[name].split('.')[0]+'.'
+
+    puts flip_name_date
+
+    start_date = Date.parse date
+    puts start_date
+
+    end_date =  Date.parse flip_name_date
+    puts end_date
+
+    date_difference = (end_date - start_date).to_i
+
+    if date_difference == 0
+      puts 'Congratulations'
+    elsif date_difference < 0
+      year = year.to_i + 1
+
+      puts Date.leap?(year)
+
+      #abort
+      flip_name_date = year.to_s + '.' + name_date[name].split('.')[1] +'.'+ name_date[name].split('.')[0]+'.'
+      start_date = Date.parse date
+      end_date =  Date.parse flip_name_date
+      date_difference = (end_date - start_date).to_i
+      puts "You have to wait #{date_difference} more days"
+    else
+      puts "You have to wait #{date_difference} more days"
+    end
+  else
+    puts 'We are sorry but we don\'t know when you should celebrate'
+  end
+end
+
+name_date(ARGV[0])
